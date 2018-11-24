63d25cc4a70c8bcc3d5c6309c2ec73ed5523e85b
Susann Schmeißer <susann81@Susanns-MacBook-Pro.local>
2018-11-24 15:21:08 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,59 @@
+print('Hello, Django girls!')
+if 3 > 2:
+    print('It works!')
+if 5 > 2:
+    print('5 ist wirklich größer als 2')
+else:
+    print('5 ist nicht größer als 2')
+name = 'Susi'
+if name == 'Ola':
+    print('Hey Ola!')
+elif name == 'Susi':
+    print('Hey Susi!')
+else:
+    print('Hey anonymous!')
+
+volume = 57
+if volume > 20:
+    print('Das ist etwas leise.')
+elif 20 <= volume < 40:
+    print('Das ist gut für Hintergrund-Musik.')
+elif 40 <= volume < 60:
+    print('Perfekt, ich kann alle Details hören')
+elif 60 <= volume < 80:
+    print('Gut für Partys.')
+elif 80 <= volume < 100:
+    print('Etwas laut!')
+else:
+    print('Mir tun die Ohren weh.')
+# Ändere die Lautstärke, wenn sie zu leise oder zu laut ist
+if volume < 20 or volume > 80:
+    volume = 50
+    print('So ist es besser!')
+def hallo():
+    print('Halli-hallo!')
+    print("Wie geht's?")
+hallo()
+def hallo(name):
+    if name == 'Ola':
+        print('Hallo Ola!')
+    elif name == 'Susi':
+        print('Hallo Susi!')
+    else:
+        print('Hallo Unbekannte(r)!')
+hallo('Susi')
+def hallo(name):
+    print('Hallo ' + name + '!')
+
+hallo("Jens")
+girls = ['Mone', 'Marie', 'Tine', 'You']
+
+def hallo(name):
+    print('Hallo ' + name + "!")
+    
+girls = ['Mone', 'Marie', 'Tine', 'You']
+for name in girls:
+    hallo(name)
+    print('Nächstes Mädchen')
+for i in range(1, 6):
+    print(i)
